ed6a6f2c3e55a1d37e849cd287b6f7c1f7c6ba77
Sunil K <sunilk.offsureit@gmail.com>
2024-09-20 18:25:03 +0530

    initial commit

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+
     id("maven-publish")
 
 }
@@ -27,6 +29,19 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+publishing{
+    publications{
+        register<MavenPublication>("release"){
+            afterEvaluate{
+                from(components["release"])
+            }
+        }
     }
 }
 
